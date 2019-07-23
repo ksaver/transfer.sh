eb03622980108b5f365845041aada32c992177c4
viktor ksaver <ks4v3r@gmail.com>
2019-07-23 00:58:43 -0500

    Create transfer.sh
    Initial commit.

---
+++ transfer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#  transfer.sh
+#  ~~~~~~~~~~~
+#  Upload files to https://transfer.sh, for sharing online.
+
+if [ -z $1 ]; then
+    echo "Uso: $(basename $0) <filename>"
+    exit 1
+else
+    FILE="$1"
+    SHARE_URL=$(curl -s --upload-file $FILE https://transfer.sh/$FILE)
+    echo -n "$SHARE_URL" | xclip -selection clipboard
+    echo "[*] URL to share: $SHARE_URL"
+    echo "(Copied to clipboard.)"
+fi
