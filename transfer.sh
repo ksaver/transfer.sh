e4a96834c61c4a9c003674c6a53251ebd50c49cf
Jesus Christian Cruz Acono <compermisos@gmail.com>
2019-08-05 12:19:25 -0500

    lint the code based in shellcode
     prevent globbing, and disable shelcheck in the usage example, now pass shellcheck

--- transfer.sh
+++ transfer.sh
@@ -4,12 +4,13 @@
 #  ~~~~~~~~~~~
 #  Upload files to https://transfer.sh, for sharing online.
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
+    # shellcheck disable=SC2086
     echo "Uso: $(basename $0) <filename>"
     exit 1
 else
     FILE="$1"
-    SHARE_URL=$(curl -s --upload-file $FILE https://transfer.sh/$FILE)
+    SHARE_URL=$(curl -s --upload-file "$FILE" "https://transfer.sh/${FILE}")
     echo -n "$SHARE_URL" | xclip -selection clipboard
     echo "[*] URL to share: $SHARE_URL"
     echo "(Copied to clipboard.)"
