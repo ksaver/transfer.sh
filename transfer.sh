f121179f14433c50d5139fd7818ce63445a79df2
viktor ksaver <ks4v3r@gmail.com>
2019-11-14 20:40:42 -0600

    Update transfer.sh
    Added some extra useless functionalities..

--- transfer.sh
+++ transfer.sh
@@ -2,15 +2,44 @@
 #
 #  transfer.sh
 #  ~~~~~~~~~~~
-#  Upload files to https://transfer.sh, for sharing online.
+#  Share files online to https://transfer.sh
+#  from command line.
+#  Requires curl in order to work and xclip optionally
 
-if [ -z $1 ]; then
-    echo "Uso: $(basename $0) <filename>"
-    exit 1
-else
-    FILE="$1"
-    SHARE_URL=$(curl -s --upload-file $FILE https://transfer.sh/$FILE)
+function upload_n_clip() {
+    SHARE_URL=$(curl -s --upload-file "$1" --url "https://transfer.sh/$1")
     echo -n "$SHARE_URL" | xclip -selection clipboard
     echo "[*] URL to share: $SHARE_URL"
-    echo "(Copied to clipboard.)"
-fi
+    echo "[*] URL copied to clipboard."
+}
+
+function upload_only() {
+    SHARE_URL=$(curl --upload-file "$1" --url "https://transfer.sh/$1")
+    echo "[*] URL to share: $SHARE_URL"
+}
+
+function main() {
+    if [ -z $1 ]; then
+        echo "basename: $(Usage $0) <filename>"
+        echo ""
+        exit 1
+    else
+        FILE="$1"
+    fi
+    
+    # curl required
+    type curl &>/dev/null \
+        || (echo "Please install curl before continue."; exit 1)
+    
+    # if xclip is not installed, upload file only
+    # upload and copy url to clipboard otherwise
+    echo "[*] Uploading \"$FILE\".."
+    type xclip &>/dev/null
+    if [ $? -eq 0 ]; then
+        upload_n_clip $FILE
+    else
+        upload_only $FILE
+    fi
+}
+
+main $1
